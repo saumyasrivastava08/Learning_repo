a0f6fbffbbab62903a9dd196868ac392d63f1e4a
Saumyanshu <saumyasanjay08@gmail.com>
2020-12-12 16:33:47 +0530

    added some icons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PageWrapper from './components/PageWrapper';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-//Pages
+ //Pages
 import Home from './components/Pages/Home';
 import About from './components/Pages/About'
 import Contact from './components/Pages/Contact'
--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -8,14 +8,14 @@ class PageWrapper extends Component{
                 
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav" style={{backgroundColor:'black'}}>
             <div className="container">
-                <Link className="navbar-brand js-scroll-trigger" to="/"><img src="assets/img/navbar-logo.svg" alt="" /></Link>
+                <Link className="navbar-brand js-scroll-trigger" to="/"><img style= {{width: '20%', height: '20%'}} src="assets/img/navbar-logo.jpg" alt="" /></Link>
                 <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     lol
                     <i className="fas fa-bars ml-1"></i>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav text-uppercase ml-auto">
-                        <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/services">Upcoming Contest</Link></li>
+                        <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/services">Upcoming Events</Link></li>
                         <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/contact">File Complaint</Link></li>
                         <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/about">Phone Number List</Link></li>
                         <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/portfolio">Photo Gallery</Link></li>
